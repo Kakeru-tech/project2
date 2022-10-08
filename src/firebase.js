71902ab6d60b4c4e043b6d12af35ce6b11505a6e
Kakeru-tech <urgex46@gmail.com>
2022-10-08 20:58:43 +1100

    saas to sass, and updated env variable name

--- src/firebase.js
+++ src/firebase.js
@@ -4,24 +4,25 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import {getFirestore} from "firebase/firestore"
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID
-// };
-
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyDZG8OejoCYlZ-HpIshpX6x_6U_kV-NalY',
-  authDomain: 'react-chat-project-c5725.firebaseapp.com',
-  projectId: 'react-chat-project-c5725',
-  storageBucket: 'react-chat-project-c5725.appspot.com',
-  messagingSenderId: '248888556843',
-  appId: '1:248888556843:web:6bb5729794a6935c6c196e'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
+//  console.log('firebase_api_key', process.env.REACT_APP_FIREBASE_API_KEY)
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDZG8OejoCYlZ-HpIshpX6x_6U_kV-NalY',
+//   authDomain: 'react-chat-project-c5725.firebaseapp.com',
+//   projectId: 'react-chat-project-c5725',
+//   storageBucket: 'react-chat-project-c5725.appspot.com',
+//   messagingSenderId: '248888556843',
+//   appId: '1:248888556843:web:6bb5729794a6935c6c196e'
+// };
 
 
 
